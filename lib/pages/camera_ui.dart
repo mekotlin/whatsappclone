9940115cde805a1649ffca6830e763022e2285d1
Dnyaneshwar Dalvi - Home <dnyaneshwar@onevoicetransmedia.com>
2019-08-13 22:33:11 +0530

    Changes

--- lib/pages/camera_ui.dart
+++ lib/pages/camera_ui.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-class CameraUI extends StatelessWidget {
+List<CameraDescription> cameras;
+
+class CameraUI extends StatefulWidget {
   @override
+  _CameraUIState createState() => _CameraUIState();
+}
+
+class _CameraUIState extends State<CameraUI> {
+  List<CameraDescription> cameras;
+  CameraController controller;
+  bool isReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+    setupCameras();
+  }
+
+  Future<void> setupCameras() async {
+    try {
+      cameras = await availableCameras();
+      controller = new CameraController(cameras[0], ResolutionPreset.medium);
+      await controller.initialize();
+    } on CameraException catch (_) {
+      setState(() {
+        isReady = false;
+      });
+    }
+    setState(() {
+      isReady = true;
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Camera"),
-    );
+    if (!isReady && !controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: CameraPreview(controller));
   }
 }
