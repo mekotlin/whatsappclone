3905eb9775d576cf8b90ed27746e91a0373b151e
Dnyaneshwar <dnyaneshwar@onevoicetransmedia.com>
2019-08-16 10:10:05 +0530

    Camera Changes

--- lib/pages/camera_ui.dart
+++ lib/pages/camera_ui.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
-
 class CameraUI extends StatefulWidget {
+  List<CameraDescription> camers;
+
+  CameraUI(this.camers);
+
   @override
   _CameraUIState createState() => _CameraUIState();
 }
 
 class _CameraUIState extends State<CameraUI> {
+  CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(widget.camers[0], ResolutionPreset.high);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+
+    return AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: CameraPreview(controller),
     );
   }
 }
